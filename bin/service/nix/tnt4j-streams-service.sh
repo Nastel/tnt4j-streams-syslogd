8e54e71ac87f70c4c0f81c39c4ed46dfe6468570
Andrius Kausinis <andrius.kausinis@singleton-labs.lt>
2020-11-04 13:44:54 +0200

    releasing version 1.11.4

--- bin/service/nix/tnt4j-streams-service.sh
+++ bin/service/nix/tnt4j-streams-service.sh
@@ -18,7 +18,7 @@ NAME=tnt4j-streams-syslogd
 EXEC="/usr/bin/jsvc"
 
 # The path to the folder containing tnt4j-streams-syslogd-*.jar
-FILE_PATH="/home/tnt4j-streams-syslogd-1.11.0/"
+FILE_PATH="/home/tnt4j-streams-syslogd-1.11.4/"
 
 PARSER_CONFIG=$FILE_PATH/config/tnt-data-source.xml
 
